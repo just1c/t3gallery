ce3bf02a935d8040aa29a7101e2ec40c63882e06
Justice Duruanyanwu <justice@Justices-MacBook-Air.local>
2024-04-19 16:21:41 +0100

    scaffold UI

--- README.md
+++ README.md
@@ -2,8 +2,8 @@
 
 ## TODO
 
-- [ ] Make it Deploy (vercel)
-- [ ] Scaffold basic UI with mock data
+- [*] Make it Deploy (vercel)
+- [*] Scaffold basic UI with mock data
 - [ ] Actually set up database (vercel postgres)
 - [ ] Attach database to UI
 - [ ] Add Authentication (w/ Clerk)
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,28 @@
 import Link from "next/link";
 
+const mockUrls = [
+  "https://utfs.io/f/194f85f3-f8ec-4770-89f3-08c8e3383f07-lqlmh8.jpg",
+  "https://utfs.io/f/33893917-831b-41f5-8cba-172e6ac11f55-m9rgai.jpg",
+  "https://utfs.io/f/0dd4c721-df78-4cb3-b430-44193b36fe73-25sfea.jpg",
+  "https://utfs.io/f/eea0742a-1c7c-44e2-b70f-d05c8cb6ed40-6mli3j.jpg",
+  "https://utfs.io/f/734c797f-230e-49bc-8474-12b88209ac98-9x7lp4.jpg"
+]
+
+const mockImages = mockUrls.map((url,index) => ({
+  id: index + 1,
+  url
+}))
+
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-    Hello (gallery in progress)
+    <main className="">
+      <div className="flex flex-wrap gap-4">{
+        [...mockImages, ...mockImages, ...mockImages].map((image) => (
+          <div key={image.id} className="w-40 p-4">
+            <img src={image.url} alt="image" />
+          </div>
+        ))
+      }</div>
     </main>
   );
 }
